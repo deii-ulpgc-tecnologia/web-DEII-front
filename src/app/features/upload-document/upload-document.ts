1e5ccc4eb2aff35f16647fa8cc379e65392737da
Eloxx7 <tracorcarmelo@gmail.com>
2026-04-16 21:20:43 +0100

    feat: upload document component

---
+++ src/app/features/upload-document/upload-document.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-document',
+  imports: [],
+  templateUrl: './upload-document.html',
+  styleUrl: './upload-document.css',
+})
+export class UploadDocument {
+
+}
