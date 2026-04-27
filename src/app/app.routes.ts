a9d196809934dfda1f1edb8ac077626804673e42
lumusa2design <lumusa2design@gmail.com>
2026-04-27 12:01:00 +0100

    FEAT: interface Documento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { NoticiaFormComponent } from './components/noticia-form/noticia-form';
 
 export const routes: Routes = [
   { path: 'noticias', component: Noticias },
-  { path: 'noticias/crear', component: NoticiaFormComponent }, // Ruta para crear
+  { path: 'noticias/crear', component: NoticiaFormComponent }, // Ruta para crear PORFI SI USAN CHATGPT AL MENOS BORREN LOS COMENTARIOS DE LOS CODIGOS QUE LES DA, NO SEAN VAGOS PORFA
   { path: 'noticias/editar/:id', component: NoticiaFormComponent }, // Ruta para editar
   { path: '', redirectTo: 'noticias', pathMatch: 'full' }
 ];
