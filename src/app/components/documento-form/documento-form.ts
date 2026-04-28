954a41b101169a87b1febd05dcf828dcd9cbebb3
lumusa2design <lumusa2design@gmail.com>
2026-04-28 12:01:00 +0100

    FEAT: added to app.routes.ts the document form

---
+++ src/app/components/documento-form/documento-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documento-form',
+  imports: [],
+  templateUrl: './documento-form.html',
+  styleUrl: './documento-form.css',
+})
+export class DocumentoForm {}
