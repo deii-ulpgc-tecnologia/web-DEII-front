913c36c46723a825b3ed5e4c674ad4f771719e90
lumusa2design <lumusa2design@gmail.com>
2026-04-30 12:01:00 +0100

    FIX: ROUTLINK

--- src/app/components/documentos/documentos.ts
+++ src/app/components/documentos/documentos.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { Documento } from '../../core/models/documento';
 
 @Component({
   selector: 'app-documentos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './documentos.html',
-  styleUrl: './documentos.css',
+  styleUrl: './documentos.css'
 })
-export class Documentos {}
+export class DocumentosComponent {
+
+  documentos: Documento[] = [
+    {
+      id: 1,
+      nombre: 'Apuntes Tema 1',
+      asignatura: 'Algoritmo y Programación',
+      carrera: 'Ingeniería Informática',
+      curso: '2º',
+      tipo: 'Teoría',
+      pdfUrl: 'assets/docs/tema1.pdf'
+    },
+    {
+      id: 2,
+      nombre: 'Práctica 2',
+      asignatura: 'ASR',
+      carrera: 'Ingeniería Informática',
+      curso: '3º',
+      tipo: 'Prácticas',
+      pdfUrl: 'assets/docs/practica2.pdf'
+    }
+  ];
+
+}
