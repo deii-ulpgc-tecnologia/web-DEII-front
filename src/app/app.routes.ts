b60a5a3f81a575e9ba5a3b0c17a487abf68dd251
Jesus Toro <jesus.tdd03@gmail.com>
2026-05-05 17:43:33 +0100

    Generated component subject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Noticias } from './components/noticias/noticias';
 import { NoticiaFormComponent } from './components/noticia-form/noticia-form';
+import { Subjects } from './components/subjects/subjects';
 
 export const routes: Routes = [
   { path: 'noticias', component: Noticias },
   { path: 'noticias/crear', component: NoticiaFormComponent }, // Ruta para crear
   { path: 'noticias/editar/:id', component: NoticiaFormComponent }, // Ruta para editar
+  { path: 'asignaturas', component: Subjects },
   { path: '', redirectTo: 'noticias', pathMatch: 'full' }
 ];
