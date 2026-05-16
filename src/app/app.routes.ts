91cb8642c257d47c053e38fc19ae13d5cb4fc018
GabriHV <gabri.hv09@gmail.com>
2026-05-16 03:11:44 +0000

    title,description,image.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Noticias } from './components/noticias/noticias';
 import { NoticiaFormComponent } from './components/noticia-form/noticia-form';
+import { AboutUs } from './components/about-us/about-us';
 
 export const routes: Routes = [
   { path: 'noticias', component: Noticias },
   { path: 'noticias/crear', component: NoticiaFormComponent }, // Ruta para crear
   { path: 'noticias/editar/:id', component: NoticiaFormComponent }, // Ruta para editar
-  { path: '', redirectTo: 'noticias', pathMatch: 'full' }
+  { path: '', redirectTo: 'noticias', pathMatch: 'full' },
+  { path: 'about-us', component: AboutUs }
 ];
