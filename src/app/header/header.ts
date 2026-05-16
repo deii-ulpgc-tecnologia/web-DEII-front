21971e377b83a11c043e58677c98b059439d1c16
lumusa2design <lumusa2design@gmail.com>
2026-05-16 11:09:34 +0100

    FIX: header bubbles problems

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,15 +1,12 @@
-import { Component, inject } from '@angular/core';
-import { Solitude } from '../core/services/solitude';
-import { RouterLink } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
-  styleUrl: './header.css',
+  styleUrl: './header.css'
 })
 export class Header {
-  solicitudesPendientes = inject(Solitude);
-  pendientes = this.solicitudesPendientes.numeroPendientes;
+  pendientes = signal(0);
 }
